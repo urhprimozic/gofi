c0a3f7b7e24b90733f610f07191ffc98f59cef40
Urh Primožič <urh.primozic@gmail.com>
2025-06-02 20:45:33 +0200

    grid update

--- gofi/ode/dihedral/experiments/1dim_grid.py
+++ gofi/ode/dihedral/experiments/1dim_grid.py
@@ -13,7 +13,7 @@ def grid(
     gradient_flow: GradientFlow,
     verbose: bool = False,
     eps=0.0001,
-    t_max=50,
+    t_max=1,
 ):
     """
     Creates resolution * resolution grid of different values for real numbers R and S.
@@ -45,7 +45,7 @@ def grid(
         r0 = r.reshape((1,1))
         s0 = s.reshape((1,1))
         
-        solution = gradient_flow.solve(r0, s0, eps=0.001, t_max=4)
+        solution = gradient_flow.solve(r0, s0, eps=eps, t_max=t_max)
         grid_dict[(r.item(), s.item())] = solution
 
     return grid_dict
@@ -58,12 +58,13 @@ if __name__ == "__main__":
     parser.add_argument("max_value", type=str, help="Max value of the grid")
     parser.add_argument("resolution", type=str, help="Grid resolution")
     parser.add_argument("filename", type=str, help="Filename to save the result")
-    parser.add_argument("--eps",default="0.001", type=str, help="Eps")
-   
+    parser.add_argument("--eps",default="0.01", type=str, help="Eps")
+    parser.add_argument("--t_max",default="1", type=str, help="Eps")
     args = parser.parse_args()
     # collect args
     n = int(args.n)
     eps = float(args.eps)
+    t_max = float(args.t_max)
     min_value = int(args.min_value)
     max_value = int(args.max_value)
     resolution = int(args.resolution)
@@ -76,7 +77,7 @@ if __name__ == "__main__":
     loss = irr_loss + unit_loss + rel_loss
     gf = GradientFlow(1, loss, clipping_limit=100)
     # run grid
-    grid_dict = grid(min_value, max_value, resolution, gf, verbose=True, eps=eps)
+    grid_dict = grid(min_value, max_value, resolution, gf, verbose=True, eps=eps, t_max=t_max)
 
     # save grid 
     with open(f'{filename}.pkl', 'wb') as f:
