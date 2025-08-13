0df191c72a7ebd0281cd7f55e39622063e9a75e8
Urh Primožič <urh.primozic@gmail.com>
2025-08-13 13:51:27 +0200

    fix sh

--- gofi/graphs/inversion_table/experiments/e2.sh
+++ gofi/graphs/inversion_table/experiments/e2.sh
@@ -2,6 +2,6 @@
 
 for n in {1..15}
 do
-    m=5*n
+    m=(( 5 * n ))
     screen -dmS "simple_$m" bash -c "source ~/venv/bin/activate && python3 -i ~/gofi/gofi/graphs/inversion_table/experiments/e2_shallow_wide.py $m --lambda_lr simple --verbose 2"
 done
