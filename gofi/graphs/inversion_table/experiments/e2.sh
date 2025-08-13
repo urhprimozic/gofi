17f883228b75f2915403e2608fabce93fc955b4f
Urh Primožič <urh.primozic@gmail.com>
2025-08-13 13:45:23 +0200

    fix vash script

--- gofi/graphs/inversion_table/experiments/e2.sh
+++ gofi/graphs/inversion_table/experiments/e2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for n in {5..50}
+for n in {1..15}
 do
-    screen -dmS "simple_$n" bash -c "source /venv/bin/activate && python3 -i /gofi/gofi/graphs/inversion_table/experiments/e2_shallow_wide.py $n --lambda_lr simple"
+    m=5*n
+    screen -dmS "simple_$m" bash -c "source ~/venv/bin/activate && python3 -i ~/gofi/gofi/graphs/inversion_table/experiments/e2_shallow_wide.py $m --lambda_lr simple --verbose 2"
 done
