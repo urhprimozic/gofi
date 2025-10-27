4c94463acf3119a1850701d053c39d28539ad770
urhprimozic <urh.primozic@gmail.com>
2025-10-27 17:24:44 +0100

    add bash

--- gofi/graphs/inversion_table/ex1.sh
+++ gofi/graphs/inversion_table/ex1.sh
@@ -9,6 +9,6 @@ for n in "${Ns[@]}"; do
   for ls in "${LSs[@]}"; do
     session_name="n_${n}_ls_${ls}"
     echo "Starting screen session: $session_name"
-    screen -dmS "$session_name" bash -c "python3 run.py $n --layer_size $ls --lr 0.0001 --T 10 --verbose 1 --grad_clipping 2 ; exec bash"
+    screen -dmS "$session_name" bash -c "bash -i -c 'source ~/venv/bin/activate && python3 run.py $n --layer_size $ls --lr 0.0001 --T 10 --verbose 1 --grad_clipping 2 ; exec bash"
   done
 done
