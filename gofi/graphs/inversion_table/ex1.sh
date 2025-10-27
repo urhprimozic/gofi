cc644c90c92dd249c58e5ea03bdc46ffab8638ac
urhprimozic <urh.primozic@gmail.com>
2025-10-27 17:29:07 +0100

    fix

--- gofi/graphs/inversion_table/ex1.sh
+++ gofi/graphs/inversion_table/ex1.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Arrays of values
-Ns=(8 16 32 64)
-#Ns=(5 6)
-LSs=(128 512 1024 2048)
-#LSs=(64 128)
+Ns=(5 6)
+LSs=(64 128)
 
-# Loop over values
 for n in "${Ns[@]}"; do
   for ls in "${LSs[@]}"; do
     session_name="n_${n}_ls_${ls}"
     echo "Starting screen session: $session_name"
-    screen -dmS "$session_name" bash -c "bash -i -c 'source ~/venv/bin/activate && python3 run.py $n --layer_size $ls --lr 0.0001 --T 10 --verbose 1 --grad_clipping 2 ; exec bash"
+    screen -dmS "$session_name" bash -c "
+      source ~/venv/bin/activate
+      python3 run.py $n --layer_size $ls --lr 0.0001 --T 10 --verbose 1 --loss_sample_size 64
+      echo 'Python command finished. Press Ctrl+A D to detach.'
+      exec bash
+    "
   done
 done
