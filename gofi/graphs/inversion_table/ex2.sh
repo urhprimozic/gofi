f2843e83d1ebf16b3c62709f908b62065bf7b2af
urhprimozic <urh.primozic@gmail.com>
2025-10-28 13:02:43 +0100

    graph matching expiriment

--- gofi/graphs/inversion_table/ex2.sh
+++ gofi/graphs/inversion_table/ex2.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-# Arrays of values
-Ns=(8 16 32 64)
-LSs=(128 512 1024 2048)
+# Ns
+Ns=(8 10 16 20 32 )
+# 64)
 
-# Loop over values
-for n in "${Ns[@]}"; do
+# Corresponding layer sizes for each N (as space-separated strings)
+LSs_list=(
+  "64"          # for N=8
+  "128 256"      # for N=10
+  "256 512"  # for N=16
+  "512"         # for N=20
+  "1024"    # for N=32
+  #"512 1024"        # for N=64
+)
+
+for i in "${!Ns[@]}"; do
+  n=${Ns[$i]}
+  LSs=(${LSs_list[$i]})  # convert string to array
+
   for ls in "${LSs[@]}"; do
-    session_name="n_${n}_ls_${ls}"
+    session_name="ex2_n_${n}_ls_${ls}"
     echo "Starting screen session: $session_name"
-    screen -dmS "$session_name" bash -c "bash -i -c 'source ~/venv/bin/activate && python3 run.py $n --layer_size $ls --lr 0.0001 --T 10 --verbose 1 --grad_clipping 2 --loss stochastic --scheduler cosine_wr; exec bash"
+    screen -dmS "$session_name" bash -c "
+      source ~/venv/bin/activate
+      python3 run.py $n --layer_size $ls --lr 0.0001 --T 10 --verbose 1 --name $session_name --loss_sample_size 64
+      echo 'Python command finished. Press Ctrl+A D to detach.'
+      exec bash
+    "
   done
 done
